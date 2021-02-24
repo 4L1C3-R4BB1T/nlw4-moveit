ffe7cb0c668b6b49ba2c5fa56b7acd7ce83252e3
Livia <livinha.guimaraes.013@gmail.com>
2021-02-24 15:21:00 -0300

    Add ExperienceBar, Profile, CompletedChallenges and Countdown components

---
+++ README.md
@@ -0,0 +1,9 @@
+# move.it
+[![NPM](https://img.shields.io/npm/l/react)](https://github.com/4L1C3-R4BB1T/nlw4-moveit/blob/main/LICENSE) 
+
+# Sobre o projeto
+move.it é um projeto construído durante a 4ª **Next Level Week**, da [Rocketseat](https://rocketseat.com.br "Site da Rocketseat").
+
+# Tecnologias Utilizadas
+- ReactJS 
+- Next.js
---
+++ moveit/src/components/Profile.tsx
@@ -0,0 +1,16 @@
+import styles from "../styles/components/Profile.module.css";
+
+export function Profile() {
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://avatars.githubusercontent.com/u/68361379?s=460&u=468efe4c1ee4ad072134e079f4c9228e0d06ecec&v=4" alt="Livia G." />
+            <div>
+                <strong>Livia G .</strong>
+                <p>
+                    <img src="icons/level.svg" alt="Level" />
+                    Level 1
+                </p>
+            </div>
+        </div>
+    );
+}
