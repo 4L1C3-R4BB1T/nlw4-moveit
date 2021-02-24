a583fba187039e3bc6e5e8930e7da05995066442
Livia <livinha.guimaraes.013@gmail.com>
2021-02-24 16:08:46 -0300

    Add ChallengeBox

--- moveit/src/components/Countdown.tsx
+++ moveit/src/components/Countdown.tsx
@@ -4,7 +4,7 @@ import styles from "../styles/components/Countdown.module.css";
 let countdownTimeout: NodeJS.Timeout;
 
 export function Countdown() {
-    const [time, setTime] = useState(0.05 * 60);
+    const [time, setTime] = useState(25 * 60);
     const [isActive, setIsActive] = useState(false);
     const [hasFinished, setHasFinished] = useState(false);
 
