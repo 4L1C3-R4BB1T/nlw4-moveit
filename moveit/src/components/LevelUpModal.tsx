7710c6436904fea1078c33b7526f248ae0b3f6fc
Livia <livinha.guimaraes.013@gmail.com>
2021-02-27 14:58:18 -0300

    Change styles and add twitter share

--- moveit/src/components/LevelUpModal.tsx
+++ moveit/src/components/LevelUpModal.tsx
@@ -6,6 +6,16 @@ import styles from "../styles/components/LevelUpModal.module.css";
 export function LevelUpModal() {
     const { level, closeLevelUpModal } = useContext(ChallengesContext);
 
+    //provisional code
+    const text = `Avancei%20para%20o%20pr%c3%b3ximo%20level!%20Agora%20sou%20level%20${level}!`;
+    const url = window.location.href;
+    const hashtags = `moveit,nlw4,rocketseat`;
+    const SHARE_URL = `https://twitter.com/share?text=${text}&url=${url}&hashtags=${hashtags}`;
+
+    function shareProgress() {
+        window.open(SHARE_URL);
+    }
+
     return (
         <div className={styles.overlay}>
             <div className={styles.container}>
@@ -17,6 +27,16 @@ export function LevelUpModal() {
                 <button type="button" onClick={closeLevelUpModal}>
                     <img src="/icons/close.svg" alt="Fechar modal" />
                 </button>
+
+                <footer>
+                    <button 
+                        type="button"
+                        className={styles.twitterShare}
+                        onClick={shareProgress}
+                    >
+                        Compartilhar no Twitter
+                    </button>
+                </footer>
             </div>
         </div>
     );
